7a70ed2596533d7bd050c97c86172961165877a4
i42 <167512912+i42com@users.noreply.github.com>
2024-04-23 12:48:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,62 @@
-var Typer = {
-    text: '',
-    accessCountimer: null,
-    index: 0,
-    speed: 2,
-    file: 'i42.txt', // Ensure this path is correct
-    accessCount: 0,
-    deniedCount: 0,
-    consoleElement: $('#console'), // Cache the jQuery object for performance
+$(document).ready(function() {
+    var Typer = {
+        text: '',
+        accessCountimer: null,
+        index: 0,
+        speed: 2,
+        file: 'i42.txt',  // Ensure this points to the correct location
+        accessCount: 0,
+        deniedCount: 0,
 
-    init: function() {
-        this.accessCountimer = setInterval(() => this.updLstChr(), 500);
-        $.get(this.file, (data) => {
-            this.text = data.slice(0, -1);
-        });
-    },
+        init: function() {
+            $.get(this.file, function(data) {
+                Typer.text = data.slice(0, Typer.text.length - 1);
+                Typer.accessCountimer = setInterval(function() {
+                    Typer.updLstChr();
+                }, 500);
+            }).fail(function() {
+                console.error("Failed to load text from file.");
+            });
+        },
 
-    content: function() {
-        return this.consoleElement.html();
-    },
+        content: function() {
+            return $('#console').html();
+        },
 
-    write: function(str) {
-        this.consoleElement.append(str);
-    },
+        write: function(str) {
+            $('#console').append(str);
+        },
 
-    addText: function(key) {
-        if (key.keyCode == 18 || key.keyCode == 20) {
-            this[key.keyCode == 18 ? 'accessCount' : 'deniedCount']++;
-            if (this[key.keyCode == 18 ? 'accessCount' : 'deniedCount'] >= 3) {
-                this[key.keyCode == 18 ? 'makeAccess' : 'makeDenied']();
+        addText: function(key) {
+            if (key.keyCode === 18 || key.keyCode === 20) {
+                this[key.keyCode === 18 ? 'accessCount' : 'deniedCount']++;
+                if (this[key.keyCode === 18 ? 'accessCount' : 'deniedCount'] >= 3) {
+                    this[key.keyCode === 18 ? 'makeAccess' : 'makeDenied']();
+                }
+            } else if (key.keyCode === 27) {
+                this.hidepop();
+            } else if (this.text) {
+                var cont = this.content();
+                if (cont.endsWith('|')) {
+                    $('#console').html(cont.slice(0, -1));
+                }
+                this.index += (key.keyCode !== 8) ? this.speed : (this.index > 0 ? -this.speed : 0);
+                var newText = this.text.substring(0, this.index).replace(/\n/g, '<br/>');
+                $('#console').html(newText);
+                $('#console').scrollTop($('#console')[0].scrollHeight);
             }
-        } else if (key.keyCode == 27) {
-            this.hidepop();
-        } else if (this.text) {
+        },
+
+        updLstChr: function() {
             var content = this.content();
-            if (content.endsWith('|')) {
-                this.consoleElement.html(content.slice(0, -1));
-            }
-            this.index += (key.keyCode != 8) ? this.speed : Math.max(0, this.index - this.speed);
-            var newText = this.text.substring(0, this.index).replace(/\n/g, '<br/>');
-            this.consoleElement.html(newText);
-            this.scrollToBottom();
-        }
-    },
+            $('#console').html(content.endsWith('|') ? content.slice(0, -1) : content + '|');
+        },
 
-    scrollToBottom: function() {
-        var scrollHeight = this.consoleElement.prop('scrollHeight');
-        this.consoleElement.scrollTop(scrollHeight);
-    },
+        makeAccess: function() { /* Define functionality */ },
+        makeDenied: function() { /* Define functionality */ },
+        hidepop: function() { /* Define functionality */ }
+    };
 
-    updLstChr: function() {
-        var content = this.content();
-        this.consoleElement.html(content.endsWith('|') ? content.slice(0, -1) : content + '|');
-    },
+    Typer.init();
 
-    hidepop: function() {
-        // Function logic to hide popup if necessary
-    },
-
-    makeAccess: function() {
-        // Logic to handle access
-    },
-
-    makeDenied: function() {
-        // Logic to handle denial
-    }
-};
-
-Typer.init();
-
-document.onkeydown = function(e) {
-    if (e.keyCode == 27) { // Fast forward text
-        Typer.index = Typer.text.length;
-    }
-    Typer.addText(e);
-};
+    $(document).on('keydown', functi
